90cd7321a3b2d1ae7258a7266c8a1dca69bf4b7e
Savindi Ranaweera <savindi.ch1995@gmail.com>
2025-03-12 13:57:42 +0530

    Update AppListener

--- src/main/java/lk/ijse/dep13/interthreadcommunications/listener/AppListener.java
+++ src/main/java/lk/ijse/dep13/interthreadcommunications/listener/AppListener.java
@@ -3,12 +3,14 @@ package lk.ijse.dep13.interthreadcommunications.listener;
 import jakarta.servlet.ServletContainerInitializer;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
+import lk.ijse.dep13.interthreadcommunications.db.MontisoriCP;
 
 import java.util.Set;
 
 public class AppListener implements ServletContainerInitializer {
     @Override
     public void onStartup ( Set<Class<?>> set, ServletContext servletContext ) throws ServletException {
-
+        MontisoriCP montisoriCP = new MontisoriCP();
+        servletContext.setAttribute("datasource", montisoriCP);
     }
 }
