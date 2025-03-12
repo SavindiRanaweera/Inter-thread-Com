48f397a436c040fd6d39050c2819a1e61bfd6bfe
Savindi Ranaweera <savindi.ch1995@gmail.com>
2025-03-12 13:56:50 +0530

    Create AppListener

---
+++ src/main/java/lk/ijse/dep13/interthreadcommunications/listener/AppListener.java
@@ -0,0 +1,14 @@
+package lk.ijse.dep13.interthreadcommunications.listener;
+
+import jakarta.servlet.ServletContainerInitializer;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+
+import java.util.Set;
+
+public class AppListener implements ServletContainerInitializer {
+    @Override
+    public void onStartup ( Set<Class<?>> set, ServletContext servletContext ) throws ServletException {
+
+    }
+}
