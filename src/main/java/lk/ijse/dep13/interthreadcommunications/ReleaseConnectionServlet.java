d28ec5acc86775a39c514ec136a1580487897706
Savindi Ranaweera <savindi.ch1995@gmail.com>
2025-03-12 13:59:23 +0530

    Implement doDelete method

--- src/main/java/lk/ijse/dep13/interthreadcommunications/ReleaseConnectionServlet.java
+++ src/main/java/lk/ijse/dep13/interthreadcommunications/ReleaseConnectionServlet.java
@@ -7,12 +7,27 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @WebServlet(name = "release-connection-servlet", urlPatterns = "/connections/*")
 public class ReleaseConnectionServlet extends HttpServlet {
     @Override
     protected void doDelete ( HttpServletRequest req, HttpServletResponse resp ) throws ServletException, IOException {
-        super.doDelete ( req, resp );
+        if (req.getPathInfo() == null || req.getPathInfo().equals("/")) {
+            releaseAllConnections(resp);
+        }else{
+            Matcher matcher = Pattern.compile("^/(?<id>[^/]+)/?(?<rest>.*)$").matcher(req.getPathInfo());
+            if (matcher.find()){
+                String id = matcher.group("id");
+                String rest = matcher.group("rest");
+                if (rest.isBlank()){
+                    releaseConnection(id,resp);
+                }else{
+                    resp.sendError(404);
+                }
+            }
+        }
     }
 
     private void releaseConnection(String id, HttpServletResponse resp) throws  IOException {
